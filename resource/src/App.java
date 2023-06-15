9b975026afe396431e510bf5d3e8cab4dc43c0a0
unknown <adi504097@gmail.com>
2023-06-15 09:35:08 +0700

    add resource file

---
+++ resource/src/App.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+       Scanner in = new Scanner(System.in);
+        
+       System.out.println("Masukan nilai awal : ");
+       int nilaiAwal = in.nextInt();
+
+        System.out.println("Masukan nilai akhir : ");
+        int nilaiAkhir = in.nextInt();
+
+        int total = 0;
+        System.out.println("Karena nilai total sebelumnya adalah : "+total);    
+        while(nilaiAwal <= nilaiAkhir){
+
+                    
+            total = nilaiAwal + total;
+
+            
+            System.out.println("Ditambah " +nilaiAwal+ " Menjadi "+total);
+            nilaiAwal++;
+        } 
+        
+    }
+}
