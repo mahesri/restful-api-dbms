69d4f223f191d27dd6f6f710d5f4b21ac272289c
unknown <adi504097@gmail.com>
2023-06-17 16:54:17 +0700

    add 3 operator

---
+++ resource/src/operasi.java
@@ -0,0 +1,18 @@
+public class operasi {
+
+    public static void main (String[]args){
+
+
+        int i = 0 ;
+
+        while(i<20){
+            if(i<8){
+                break;
+            }
+    System.out.print("perulangan ke - : "+i);
+    i++;
+        }
+
+    }
+    
+}
