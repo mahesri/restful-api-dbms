50467a0e28becf2feb726a6ba1d3ad74dda936cb
mahesri <adi504097@gmail.com>
2023-09-24 09:47:46 +0700

    juragan dateng

--- resource/src/tugas2.java
+++ resource/src/tugas2.java
@@ -6,3 +6,37 @@ class formatBiodata {
     float nilaiUts;
     float nilaiUas;
 }
+
+public class tugas2 {
+
+    private static Scanner in = new Scanner(System.in);
+    private static int jmlMhs = 0;
+
+    public static void inputData(formatBiodata biodataMahasiswa[]){
+
+        System.out.println("Masukan jumlah mahasiswa : ");
+        jmlMhs
+        for(int i = 0;  )
+    }
+
+public static void main(String[]args){
+    int pilihan = 1;
+
+
+    do {
+        System.out.println("|--------------MENU---------------|");
+        System.out.println("| PILIH |1 INPUT| |2 VIEW| |3 EXIT|");
+        System.out.println("|---------------------------------|");
+        System.out.print("Silahkan masukan pilihan Anda : ");
+        pilihan = in.nextInt();
+
+        switch(pilihan){
+            case 1 :
+
+        }
+        
+    }while(pilihan< 3);
+
+}
+
+}
