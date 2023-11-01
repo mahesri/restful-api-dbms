bd55aa37833a95a54fcf7445ab299306a1191160
mahesri <adi504097@gmail.com>
2023-11-02 00:29:12 +0700

    pulsh

--- LapRak5/mod5pert5/src/pert5_1.java
+++ LapRak5/mod5pert5/src/pert5_1.java
@@ -3,4 +3,9 @@ public static void mengurutkanDataBubble(formatBiodata biodataMahasiswa[]){
 	formatBiodata biodataMahasiswa = new formatBiodata();
 
 	int indexTerakhir = N -1;
+	for(int j = 0;j <= indexsTerakhir - 1; j++ ){
+
+			for(int i = 0;)
+
+		}
 	}
