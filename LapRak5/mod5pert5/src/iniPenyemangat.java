cf9e8dbcfb0bfa206d4817f999adb5587acc2edf
mahesri <adi504097@gmail.com>
2023-11-04 16:48:18 +0700

    okey it's just't begin

---
+++ LapRak5/mod5pert5/src/iniPenyemangat.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class iniPenyemangat{
+
+	public static void main (String[]args){
+
+		Scanner in = new Scanner(System.in);
+
+System.out.println("Masukan bilang Anda : ");
+int bilangan = in.nextInt();
+
+if(bilangan % 2 == 0){
+
+	System.out.println("Bilangan Genap");
+	}else{
+		System.out.println("Bilangan ganjil");
+		}
+
+		}
+		}
