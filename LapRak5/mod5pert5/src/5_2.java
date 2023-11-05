0434ef378f4802908b4d988c0376c15a2ffbb300
mahesri <adi504097@gmail.com>
2023-11-05 21:43:02 +0700

    fokus

--- LapRak5/mod5pert5/src/5_2.java
+++ LapRak5/mod5pert5/src/5_2.java
@@ -1,14 +1,30 @@
 public static void mengurutkanDataSelection(formatBiodata biodataMahasiswa[]){
+	
+	formatBiodata biodataSementara = new formatBiodata();
 
-formatBiodata biodataSementara = new formatBiodata();
+	String teksTerkecil = "";
+	int lokasi = 0;
 
-String teksTerkecil = "";
-int lokasi = 0;
+	for (int i = 0 ; i <= N - 2;i++){
 
-for(int i = 0; i <N - 2; i++){
+		teksTerkecil = "zzzzzzz";
 
-teksTerkecil = "zzzzzzzzzz"
+		for (int S = i + 1; S <= N-1;S++) {
+
+			if(biodataMahasiswa[S].nama.compareTo(teksTerkecil)<0){
+				teksTerkecil = biodataMahasiswa[S].nama;
+				lokasi = S;
+			}
+		}
+		if(biodataMahasiswa[i].nama.compareTo(biodataMahasiswa[lokasi].nama) > 0){ {
+
+			biodataSementara = biodataMahasiswa[i];
+			biodataMahasiswa[i] = biodataMahasiswa[i + 1];
+			biodataMahasiswa[i+1] = biodataSementara;
+		}
+
+		}
 
 	}
 
-	}
+}
--- LapRak5/mod5pert5/src/Pemanasan.java
+++ LapRak5/mod5pert5/src/Pemanasan.java
@@ -2,38 +2,37 @@ import java.util.*;
 
 public class Pemanasan {
 
-	public static void main (String[]args){
+	public static void main(String[] args) {
 
 		Scanner in = new Scanner(System.in);
 		boolean sikon = false;
 
-		do{
-		System.out.print("Masukan bilangan : ");
-		int bil = in.nextInt();
+		do {
+			System.out.print("Masukan bilangan : ");
+			int bil = in.nextInt();
 
-		if(bil % 2 == 0){
+			if (bil % 2 == 0) {
 
-			System.out.println("Bilangan Genap!");
+				System.out.println("Bilangan Genap!");
 
-			}else{
+			} else {
 
 				System.out.println("Bilangan Ganjil!");
 
-				}
+			}
 
-				System.out.println("Ingin melakukan pengecekan lagi? (Y/N) :");
-				String jawaban = in.next();
+			System.out.println("Ingin melakukan pengecekan lagi? (Y/N) :");
+			String jawaban = in.next();
 
-				if(jawaban.equals("Y")){
+			if (jawaban.equalsIgnoreCase("Y")) {
 
-					sikon = true;
-					}else{
-						sikon = false;
-						}
+				sikon = true;
+			} else {
+				sikon = false;
+			}
 
-				}while(sikon);
-
-
-		}
+		} while (sikon);
 
 	}
+
+}
