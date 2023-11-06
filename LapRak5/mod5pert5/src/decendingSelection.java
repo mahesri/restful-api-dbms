27e659cb794346290fc1f3bff1997579c5ba14fb
mahesri <adi504097@gmail.com>
2023-11-06 21:43:42 +0700

    FSS

--- LapRak5/mod5pert5/src/decendingSelection.java
+++ LapRak5/mod5pert5/src/decendingSelection.java
@@ -1,3 +1,17 @@
-public class decendingSelection {
-    
-}
+
+ public static void urutDataSelectionDecending(formatBiodata biodataMahasiswa[]){
+
+    formatBiodata biodataSememntara = new biodataMahasiswa[];
+
+    String teksTerbesar = "";
+
+    int lokasi;
+
+    for(int i = 0; i <= N-1;i++ ){
+
+
+
+    }
+
+    }
+
