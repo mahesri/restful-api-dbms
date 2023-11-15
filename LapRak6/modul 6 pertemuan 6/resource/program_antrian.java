d3b34b356061bdafc45c407088621f7640b5ddc8
mahesri <adi504097@gmail.com>
2023-11-15 20:35:32 +0700

    FSS

--- LapRak6/modul 6 pertemuan 6/resource/program_antrian.java
+++ LapRak6/modul 6 pertemuan 6/resource/program_antrian.java
@@ -9,9 +9,45 @@ if(belakang == N - 1) {//kondisi jika antrian / array penuh
 
 	System.out.println("Antrian penuh! ENQUEUE "+ data + " tidak dapat dilakukan");
 
-	}else{//kondisi jika antrian/array bisa diisi
-		belakang + belakang + 1;
+	}else{ //kondisi jika antrian/array bisa diisi
+		belakang = belakang + 1;
 		antrian[belakang] = data;
+		System.out.println("ENQUEUE " +data+ " berhasil..");
 		}
 	}
+
+public static void DEQUEUE (String antiran[]){
+
+String hasil;
+if(belakang < 0){//kondisi jika antrian kosong
+
+hasil = "ANTRIAN KOSONG, DEQUEUE GAGAL DILAKUKAN";
+}else{ //kondisi jika antrian tidak kosong
+hasil = antrian[0];
+
+//--Perulangan untuk menggeser data kedua sampai data ke-N habis tergeser kedepan
+for(int i = 0; i <= belakang-1 ; i++){
+	antrian[i] = antrian[i+1];
 	}
+belakang = belakang - 1;
+	}
+return(hasil);
+}
+
+public static void lihatAntrian(String antrian[]){
+
+System.out.println("------------------------------------------");
+System.out.print("ISI ANTRIAN : (depan)");
+for (int i=0; i<=belakang; i++){
+	System.out.print(" " + antrian[i]);
+}
+System.out.println(" (belakang)");
+System.out.println("------------------------------------------");
+	}
+public static void main (String[] args){
+	String antrian[] = new String[5];
+	ENQUEUE (antrian, "Mobil A");
+	ENQUEUE (antrian, "Mobil B");
+	ENQUEUE (antrian, "Mobil C");
+	lihatAntrian(antrian);
+}}
--- LapRak6/modul 6 pertemuan 6/resource/program_tumpukan.java
+++ LapRak6/modul 6 pertemuan 6/resource/program_tumpukan.java
@@ -1,9 +1,6 @@
-
-public class 6_1 {
-
+public class program_tumpukan {
 	public static int N = 5;
 	public static int atas = -1;
-
 public static void PUSH (String tumpukan[], String data){
 
 	if(atas == N-1) //Kondisi Jika Tumpukan Penuh
@@ -34,27 +31,29 @@ public static String POP (String tumpukan[])
 
 public static void lihatTumpukan(String tumpukan[]){
 
-	System.out.print("");
+	System.out.print(" ");
 	System.out.print("--TUMPUKAN--");
 
-	for(int i = atas; i>=0;i++){
-
+	for(int i = atas; i>=0;i--){
 		System.out.println(tumpukan[i]);
-
-		}
+	}
 System.out.println("--AKHIR TUMPUKAN--");
-System.out.println("");
+System.out.println(" ");
 	}
 
 public static void main(String[]args){
 
 	String tumpukan[] = new String[10];
-
-	.............;
-	.............;
-	.............;
-	.............;
-
-	}
-
-	}
+	PUSH (tumpukan, "Buku A");
+	PUSH (tumpukan, "Buku B");
+	PUSH (tumpukan, "Buku C");
+	PUSH (tumpukan, "Buku D");
+	PUSH (tumpukan, "Buku E");
+	PUSH (tumpukan, "Buku F");
+	PUSH (tumpukan, "Buku G");
+	System.out.println("POP: " + POP(tumpukan));
+	System.out.println("POP: " + POP(tumpukan));
+	System.out.println("POP: " + POP(tumpukan));
+	System.out.println("POP: " + POP(tumpukan));
+	lihatTumpukan(tumpukan);
+	}}
