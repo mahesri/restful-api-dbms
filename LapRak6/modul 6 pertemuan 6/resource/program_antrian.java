82a3d99733c5d63f20633edb990ff405c71c5e2d
mahesri <adi504097@gmail.com>
2023-11-19 21:06:14 +0700

    add dokumentation file

--- LapRak6/modul 6 pertemuan 6/resource/program_antrian.java
+++ LapRak6/modul 6 pertemuan 6/resource/program_antrian.java
@@ -1,53 +1,65 @@
 public class program_antrian {
 
-private static int N = 5 ;
-private static int belakang = -1;
+    private static int N = 5;
+    private static int belakang = -1;
 
-public static void ENQUEUE (String antrian[], String data){
+    public static void ENQUEUE(String antrian[], String data) {
+        if (belakang == N - 1) {
+            System.out.println("Antrian penuh! ENQUEUE " + data + " tidak dapat dilakukan");
+        } else {
+            belakang = belakang + 1;
+            antrian[belakang] = data;
+            System.out.println("ENQUEUE " + data + " berhasil..");
+        }
+    }
 
-if(belakang == N - 1) {//kondisi jika antrian / array penuh
+    public static String DEQUEUE(String antrian[]) {
+        String hasil;
+        if (belakang < 0) {
+            hasil = "ANTRIAN KOSONG, DEQUEUE GAGAL DILAKUKAN";
+        } else {
+            hasil = antrian[0];
 
-	System.out.println("Antrian penuh! ENQUEUE "+ data + " tidak dapat dilakukan");
+            // Perulangan untuk menggeser data kedua sampai data ke-N habis tergeser kedepan
+            for (int i = 0; i < belakang; i++) {
+                antrian[i] = antrian[i + 1];
+            }
+            belakang = belakang - 1;
+        }
+        return hasil;
+    }
 
-	}else{ //kondisi jika antrian/array bisa diisi
-		belakang = belakang + 1;
-		antrian[belakang] = data;
-		System.out.println("ENQUEUE " +data+ " berhasil..");
-		}
-	}
+    public static void lihatAntrian(String antrian[]) {
+        System.out.println("------------------------------------------");
+        System.out.print("ISI ANTRIAN : (depan)");
 
-public static void DEQUEUE (String antiran[]){
+        for (int i = 0; i <= belakang; i++) {
+            System.out.print(" " + antrian[i]);
+        }
 
-String hasil;
-if(belakang < 0){//kondisi jika antrian kosong
+        System.out.println(" (belakang)");
+        System.out.println("------------------------------------------");
+    }
 
-hasil = "ANTRIAN KOSONG, DEQUEUE GAGAL DILAKUKAN";
-}else{ //kondisi jika antrian tidak kosong
-hasil = antrian[0];
+    public static void main(String[] args) {
+		String antrian[] = new String[5];
+		ENQUEUE(antrian, "Mobil A");
+        ENQUEUE(antrian, "Mobil B");
+        ENQUEUE(antrian, "Mobil C");
+        ENQUEUE(antrian, "Mobil D");
+		ENQUEUE(antrian, "Mobil E");
+        ENQUEUE(antrian, "Mobil F");
+        System.out.println("deQueue: " + DEQUEUE(antrian));
+        System.out.println("deQueue: " + DEQUEUE(antrian));
+        System.out.println("deQueue: " + DEQUEUE(antrian));
+        System.out.println("deQueue: " + DEQUEUE(antrian));
+        System.out.println("deQueue: " + DEQUEUE(antrian));
+		lihatAntrian(antrian);
 
-//--Perulangan untuk menggeser data kedua sampai data ke-N habis tergeser kedepan
-for(int i = 0; i <= belakang-1 ; i++){
-	antrian[i] = antrian[i+1];
-	}
-belakang = belakang - 1;
-	}
-return(hasil);
-}
+        // Perbaikan: Menggunakan nilai yang dikembalikan dari DEQUEUE dan mencetaknya
+        String hasilDequeue = DEQUEUE(antrian);
+        System.out.println("DEQUEUE: " + hasilDequeue);
 
-public static void lihatAntrian(String antrian[]){
-
-System.out.println("------------------------------------------");
-System.out.print("ISI ANTRIAN : (depan)");
-for (int i=0; i<=belakang; i++){
-	System.out.print(" " + antrian[i]);
-}
-System.out.println(" (belakang)");
-System.out.println("------------------------------------------");
-	}
-public static void main (String[] args){
-	String antrian[] = new String[5];
-	ENQUEUE (antrian, "Mobil A");
-	ENQUEUE (antrian, "Mobil B");
-	ENQUEUE (antrian, "Mobil C");
-	lihatAntrian(antrian);
-}}
+        // Menampilkan antrian setelah DEQUEUE
+        lihatAntrian(antrian);
+    }}
