d99e9a8fbfe5c39ec5b9db073f9c17f4eefbcd0c
mahesri <adi504097@gmail.com>
2023-12-29 19:47:40 +0700

    add depedency

---
+++ src/main/java/restfulapidbms/RestfulApiDbmsApplication.java
@@ -0,0 +1,13 @@
+package restfulapidbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiDbmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiDbmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/restfulapidbms/RestfulApiDbmsApplicationTests.java
@@ -0,0 +1,13 @@
+package restfulapidbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiDbmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
