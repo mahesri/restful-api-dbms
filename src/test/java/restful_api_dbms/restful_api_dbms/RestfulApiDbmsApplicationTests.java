ffb9b06af1c13c9e5e64737e8f145a941dfdf42c
mahesri <adi504097@gmail.com>
2023-12-30 08:00:45 +0700

    add dependency

---
+++ src/test/java/restful_api_dbms/restful_api_dbms/RestfulApiDbmsApplicationTests.java
@@ -0,0 +1,13 @@
+package restful_api_dbms.restful_api_dbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiDbmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
