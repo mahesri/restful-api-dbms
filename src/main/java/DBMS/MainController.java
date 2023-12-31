e8a833df39c2ead21c8368ee32407bfcc478e62e
mahesri <adi504097@gmail.com>
2024-01-01 01:30:16 +0700

    add configuration JDBC & get mapping

--- src/main/java/DBMS/MainController.java
+++ src/main/java/DBMS/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping(value="/index") //we got an error from here
+    @GetMapping(value=" ") //we got an error from here
     public String showHomePage(){
         return"index";
     }
