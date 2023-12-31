3aa7251368cbd1ca34684faff4c2e3b2efdf38f4
mahesri <adi504097@gmail.com>
2024-01-01 00:56:35 +0700

    add dependency

---
+++ src/main/java/DBMS/MydbmsApplication.java
@@ -0,0 +1,13 @@
+package DBMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydbmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydbmsApplication.class, args);
+	}
+
+}
