98468687d7886758477ec6134f0512258af78a2a
mahesri <adi504097@gmail.com>
2024-04-27 20:36:48 +0700

    adding delete function

--- src/main/java/DBMS/user/UserService.java
+++ src/main/java/DBMS/user/UserService.java
@@ -25,13 +25,13 @@ public class UserService {
        if (result.isPresent()){
            return result.get();
        }
-       throw new UserNotFondException("Tidak dapat menemukan pengguna dengan ID" + id);
+       throw new UserNotFondException("Cannot find any user with ID " + id);
     }
 
     public void delete(Integer id) throws UserNotFondException{
     Long count = repo.countById(id);
     if (count == null || count == 0){
-        throw new UserNotFondException("Tidak dapat menemukan pengguna dengan ID" + id);
+        throw new UserNotFondException("Cannot find any user with ID " + id);
     }
     repo.deleteById(id);
     }
